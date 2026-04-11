d824fb613a17c2952f0b8b32c4a4cb1091cc766e
FlashAdking <aditya.tiwade8080@gmail.com>
2026-04-11 21:39:53 +0530

    Leetcode 3895. Count Digit Appearances

---
+++ LC3895_CountFreq.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countDigitOccurrences(int[] nums, int digit) {
+        int count = 0;
+
+        for(int i=0 ; i < nums.length ; i++){
+            int n = nums[i];
+            while( n > 0){
+                int val = n % 10;
+                if(val == digit){
+                    count++;
+                }
+                n /= 10;
+            }
+        }
+
+
+        return count;
+    }
+}
